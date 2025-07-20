64606cb125bae558844a049386ba2fb00c6d1f74
rcmiller01 <robertmillers@gmail.com>
2025-07-19 21:41:34 -0400

    Fix import issues: Add missing __init__.py files and correct class imports for all new packages

---
+++ backend/ritual/__init__.py
@@ -0,0 +1,6 @@
+# ritual package
+# Mirror Ritual system for Lyra's identity arc and transformation
+
+from .mirror_ritual import MirrorRitualEngine, MirrorRitual, RitualPhase, MirrorState, IdentityAspect
+
+__all__ = ['MirrorRitualEngine', 'MirrorRitual', 'RitualPhase', 'MirrorState', 'IdentityAspect'] 
---
+++ backend/scenes/__init__.py
@@ -0,0 +1,6 @@
+# scenes package
+# Scene Initiation and video generation system
+
+from .scene_initiation import SceneInitiationEngine, ScenePrompt, SceneType
+
+__all__ = ['SceneInitiationEngine', 'ScenePrompt', 'SceneType'] 
